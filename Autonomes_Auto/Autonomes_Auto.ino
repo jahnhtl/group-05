f6ccae5211315b807e7fbcd098fbc3e537b4d6b6
Blagoje1 <170506@htlw10.at>
2023-12-11 12:01:39 +0100

    Code hinzugefügt (kann geradeaus fahren und links und rechts teilweise)

---
+++ Autonomes_Auto/Autonomes_Auto.ino
@@ -0,0 +1,122 @@
+// Pin-Definitionen für Motoren
+#define motor1pin1 8
+#define motor1pin2 7
+#define motor2pin1 5
+#define motor2pin2 4
+
+// Pin-Definitionen für IR-Sensoren
+#define leftIR A0
+#define centerIR A1
+#define rightIR A2
+
+// Schwellenwerte für die IR-Sensoren (passen Sie diese an Ihre Bedürfnisse an)
+#define threshold1 400
+#define threshold2 250
+
+// PWM-Geschwindigkeitspin-Definitionen
+#define pwmMotor1 9
+#define pwmMotor2 6
+
+// Pin-Definition für den Button
+#define buttonPin 2
+
+// Geschwindigkeiten für die Motoren (passen Sie diese an Ihre Bedürfnisse an)
+#define baseSpeed 150
+#define turnSpeed 100
+
+volatile bool isRunning = false;
+
+void setup() {
+  // Initialisierung der seriellen Kommunikation
+  Serial.begin(9600);
+
+  // Motor-Pins als Ausgänge festlegen
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+
+  // PWM-Pins als Ausgänge festlegen
+  pinMode(pwmMotor1, OUTPUT);
+  pinMode(pwmMotor2, OUTPUT);
+
+  // Button-Pin als Eingang mit Pull-Up-Widerstand
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Attach Interrupt für den Button
+  attachInterrupt(digitalPinToInterrupt(buttonPin), toggleCar, FALLING);
+}
+
+void loop() {
+  if (isRunning) {
+    // Lesen Sie die Werte der IR-Sensoren
+    int leftValue = analogRead(leftIR);
+    int centerValue = analogRead(centerIR);
+    int rightValue = analogRead(rightIR);
+
+    // Entscheidungen basierend auf den IR-Sensorwerten
+    if (centerValue < threshold1) {
+      // Geradeaus fahren
+      driveForward();
+    } 
+    else {
+      if (rightValue > threshold2) {
+        turnLeft();
+      }     
+      else if (leftValue > threshold2) {
+        turnRight ();
+      }
+    }
+  }
+  
+ }
+
+// Funktionen für die Fahrzeugbewegungen
+void driveForward() {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(pwmMotor1, baseSpeed);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(pwmMotor2, baseSpeed);
+}
+
+
+
+void turnLeft() {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  analogWrite(pwmMotor1, turnSpeed);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(pwmMotor2, turnSpeed);
+}
+
+void turnRight() {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(pwmMotor1, turnSpeed);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  analogWrite(pwmMotor2, turnSpeed);
+}
+
+// Funktion zum Anhalten beider Motoren
+void stopMotors() {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(pwmMotor1, 0);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(pwmMotor2, 0);
+}
+
+// Interrupt Service Routine für den Button (Starten/Stoppen)
+void toggleCar() {
+  isRunning = !isRunning;
+  delay(1000); // Debouncing-Zeit
+}
